8e008d08b752f14edb3480a221a81207a95acc71
Richard Mendes Madureira <richard.madureira@dataprev.gov.br>
2022-11-30 13:39:03 -0300

    adicionado / no final da propriedade base

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://richardmadureira.github.io/teste-deploy"
+  base: "https://richardmadureira.github.io/teste-deploy/"
 })
